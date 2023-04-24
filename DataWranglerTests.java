66455c0c18657334da0d6c47700382fc29e13d53
Viola Vinatzer <vinatzer@wisc.edu>
2023-04-24 01:39:56 -0500

    Added Makefile and initial Testing file

---
+++ DataWranglerTests.java
@@ -0,0 +1,38 @@
+// --== CS400 File Header Information ==00
+// Name: Viola Vinatzer
+// Email: vinatzer@wisc.edu
+// Group and Team: DX red
+// Group TA: April Roszkowski
+// Lecturuer: Florian Heimerl
+// Notes to Grader: None
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * DataWrangler testing class
+ */
+public class DataWranglerTests{
+
+	@Test
+	public void test(){
+	}
+
+	@Test
+	public void test2(){
+	}
+
+	@Test
+	public void test3(){
+	}
+
+	@Test
+	public void test4(){
+	}
+
+	@Test
+	public void test5(){
+	}
+
+}
--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
+runDataWranglerTests: DataWranglerTests.class
+	java -jar junit5.jar -cp . --select-class=DataWranglerTests
 
+DataWranglerTests.class: DataWranglerTests.java
+	javac -cp .:junit5.jar DataWranglerTests.java
 
 clean:
 	rm *.class
