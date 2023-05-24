32ea1d0e4f3f448532dea2d006d28b9eb2e18f19
“DiegoWu” <930404d@gmail.com>
2023-05-24 23:36:22 +0800

    remove unnecessary files1

--- fileReaderInterface.java
+++
@@ -1,9 +0,0 @@
-public interface FlightPathBackendInterface {
-	// public FligthPathBackend(DijkstraWithMinTransferInterface dijkstraGraph, FileReaderInterface reader);
-	public void loadMap(String filepath) throws FileNotFoundException;
-	public List<AirportInterface> getAllAirports();
-	public List<AirportInterface> shortestPath(AirportInterface start, AirportInterface end) throws NoSuchElementException;
-	public int arrivalTime(AirportInterface start, AirportInterface end) throws NoSuchElementException;
-	public boolean directFlight(AirportInterface start, AirportInterface end); 
-}
-
