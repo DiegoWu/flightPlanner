e5143b5f17f537546ad2cbad4285817c39aff97c
“DiegoWu” <930404d@gmail.com>
2023-05-24 23:41:40 +0800

    remove unnecessary files

--- FlightPathBackendBD.java
+++ FlightPathBackendBD.java
@@ -1,12 +1,10 @@
 public class FlightPathBackendBD{
   private DijstraGraph<AirportInterface, Double> graph;
   private  DijkstraWithMinTransfer<AirportInterface, Double>() min= new DijkstraWithMinTransfer<AirportInterface, Double> (this.graph);
-
+ 
   public int arrivalTime(AirportInterface start, AirportInterface end, int time){
 
-
     return min.minTransfer(start, end);
-
 
   }
   public boolean directFlight(AirportInterface st, AirportInteface end){
