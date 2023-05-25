66a888aea9cc1d9e7bebd6cdb1f9a2cb7b6cf359
“DiegoWu” <930404d@gmail.com>
2023-05-25 11:55:18 +0800

    update backend

--- FlightPathBackendBD.java
+++ FlightPathBackendBD.java
@@ -1,9 +1,17 @@
 public class FlightPathBackendBD{
-  private DijstraGraph<AirportInterface, Double> graph;
-  private  DijkstraWithMinTransfer<AirportInterface, Double>() min= new DijkstraWithMinTransfer<AirportInterface, Double> (this.graph);
- 
-  public int arrivalTime(AirportInterface start, AirportInterface end, int time){
 
+  private fileReaderInterface reader= fileReaderDW("DWTest2.dot");
+  private DijstraGraph<AirportInterface, EdgeInterface> graph;
+  for(AirportInterface airport: reader.getAirportList()){
+    graph.insertNode(airport);
+  }
+  for(EdgeInterface edge: reader.getEdgeList()){
+    graph.insertEdge(edge.getPredecessor(), edge.getSuccessor(), edge.getWeight());
+  }
+  private  DijkstraWithMinTransfer<AirportInterface, EdgeInterface>() min= new DijkstraWithMinTransfer<AirportInterface, EdgeInterface> (this.graph);
+
+  public int arrivalTime(AirportInterface start, AirportInterface end, int time){
+    
     return min.minTransfer(start, end);
 
   }
